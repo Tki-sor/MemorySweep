21072fa8d15bfc4e2716640a4922ec1d5da0f7ef
Tki-sor <78548154+Tki-sor@users.noreply.github.com>
2023-01-13 16:31:35 +0800

    fix config gen

--- src/main/java/com/tkisor/memorysweep/MemorySweep.java
+++ src/main/java/com/tkisor/memorysweep/MemorySweep.java
@@ -1,8 +1,13 @@
 package com.tkisor.memorysweep;
 
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(MemorySweep.MODID)
 public class MemorySweep {
     public static final String MODID = "memorysweep";
+    public MemorySweep() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONFIG);
+    }
 }
